a2dd22439305e13a445bdcb0f5665639933084a2
Josh <38708953+joshmackay@users.noreply.github.com>
2024-06-25 14:01:52 +1000

    fixed date picker button

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import "./task-pane.css"
 import "./detail-pane.css"
 import "./sidebar.css"
 import "./todo.css"
-
+console.log('in')
 
 document.addEventListener("DOMContentLoaded", () => {
+    console.log("in again")
     const controller = new Controller()
     controller.initialise();
 })
